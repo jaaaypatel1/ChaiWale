3be52912e8ea8e710600d83862e387901f9789fd
jaaaypatel1 <jaylupatel13@gmail.com>
2024-02-17 22:22:45 -0500

    Attempt to make it so that it does not need a super stable mouse

--- clickonhold.py
+++ clickonhold.py
@@ -1,25 +1,21 @@
 import pyautogui
 import time
 
-
-x, y = pyautogui.position()
-
+last_click_x, last_click_y = pyautogui.position()
 
 start_time = time.time()
 
 while True:
-    
-    if pyautogui.position() != (x, y):
-        
-        x, y = pyautogui.position()
-        start_time = time.time()
-    else:
-        
+    current_x, current_y = pyautogui.position()
+    distance = ((current_x - last_click_x) ** 2 + (current_y - last_click_y) ** 2) ** 0.5  # Euclidean distance
+
+    if distance <= 100:  # Check if within 100 pixels of last click position
         if time.time() - start_time >= 2:
-            
             pyautogui.click()
-           
             start_time = time.time()
+    else:
+        last_click_x, last_click_y = current_x, current_y
+        start_time = time.time()
 
     # Check every 0.1 second
     time.sleep(0.1)
