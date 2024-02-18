f42ff1c3a754d568d5a4b4cb750284fe2c0f369e
Manasi <86559162+ms2023@users.noreply.github.com>
2024-02-17 22:50:11 -0500

    Made the keys bigger, capslock is still broken

--- keyboardFunctionality.js
+++ keyboardFunctionality.js
@@ -2,7 +2,7 @@ let textContainer = document.querySelector(".textContainer");
 let enterKey = document.querySelector(".enter");
 let spaceKey = document.querySelector(".space");
 let deleteKey = document.querySelector(".delete");
-let capsLockKey = document.querySelector(".CapsLock")
+let capsLockKey = document.querySelector(".capsLock")
 let allKeys = document.querySelectorAll(".key");
 let isCaps = false;
 
@@ -24,12 +24,12 @@ deleteKey.addEventListener("click", function(){
     let newContent = content.slice(0, content.length-1);
     textContainer.innerText = newContent;
 });
-capsLockKey.addEventListener("click", function(){
+capsLockKey.addEventListener('click', function(){
     if(isCaps){
-        CapsLock.classList.remove("active");
+        capsLock.classList.remove("active");
         
     }else{
-        CapsLock.classList.add("active");
+        capsLock.classList.add("active");
         for(let key of allKeys){
             if(key.classList.length > 1){
                 //do nothing 
