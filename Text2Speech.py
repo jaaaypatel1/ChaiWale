83a768f8a908c427cf46357c8f0afb67b079604d
KamathN593 <nk2017@kamaths.com>
2024-02-17 19:48:04 -0500

    Text2Speech feature. Pass a string to text_to_speech.

---
+++ Text2Speech.py
@@ -0,0 +1,16 @@
+from gtts import gTTS
+import os
+
+def text_to_speech(text):
+    # Initialize gTTS with the text to convert
+    speech = gTTS(text)
+
+    # Save the audio file to a temporary file
+    speech_file = 'speech.mp3'
+    speech.save(speech_file)
+
+    # Play the audio file
+    os.system('start ' + speech_file)
+    
+    
+text_to_speech('Hello, world! This is a test.')
